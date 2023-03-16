074d7b12a3ac858f096d80e65c76a38f708c7b56
mrguatno <62753382+mrguatno@users.noreply.github.com>
2023-03-16 10:20:19 +0800

    added vectors

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var config = {
     style: 'mapbox://styles/mrguatno/clfaffc50002r01s0qmpe1szp',
     accessToken: 'pk.eyJ1IjoibXJndWF0bm8iLCJhIjoiY2xlbGhuMjF0MHYwcTN2cW5rbDlnd2NzayJ9.U4E6vdDNyk8s7yoXQc6tHg',
-    showMarkers: true,
+    showMarkers: false,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
+    inset: false,
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
@@ -18,25 +18,25 @@ var config = {
         {
             id: 'slug-style-id',
             alignment: 'left',
-            hidden: false,
+            hidden: true,
             title: 'Display Title',
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
+                center: [124.1637, 9.9],
+                zoom: 9.8,
+                pitch: 0,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
+                {
+                    layer: 'consolidated',
+                    opacity: 1,
+                    duration: 5000
+                }
             ],
             onChapterExit: [
                 // {
@@ -48,13 +48,13 @@ var config = {
         {
             id: 'second-identifier',
             alignment: 'right',
-            hidden: false,
+            hidden: true,
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
+                center: [124.02634, 9.65241],
+                zoom: 11,
                 pitch: 60,
                 bearing: -43.2,
                 // flyTo additional controls-
@@ -68,25 +68,37 @@ var config = {
             rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
-            onChapterExit: []
+            onChapterExit: [
+                {
+                    layer: 'road',
+                    opacity: 0,
+                }
+            ]
         },
         {
             id: 'third-identifier',
             alignment: 'left',
-            hidden: false,
+            hidden: true,
             title: 'Third Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
+                center: [124.1637, 9.9],
+                zoom: 9.8,
+                pitch: 0,
                 bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
+            onChapterEnter: [
+                    {
+                        layer: 'consolidated',
+                        opacity: 1,
+                        duration: 5000
+                    }
+
+            ],
             onChapterExit: []
         },
         {
